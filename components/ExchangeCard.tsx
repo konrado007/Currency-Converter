0bb6f310b466447bb237dd99efe6688cb0ac5415
konrado007 <konrado1@o2.pl>
2023-08-10 00:32:57 +0200

    converter added

---
+++ components/ExchangeCard.tsx
@@ -0,0 +1,97 @@
+import { symbols } from "@/constants/currencyRates";
+import React, { useState, useEffect } from "react";
+import { CgArrowsExchangeAltV } from "react-icons/cg";
+import { IoIosArrowDown } from "react-icons/io";
+
+interface ExchangeCardProps {
+  label: string;
+  down?: boolean;
+  calculatedCurrency?: number;
+}
+
+const ExchangeCard: React.FC<ExchangeCardProps> = ({
+  label,
+  down,
+  calculatedCurrency,
+}) => {
+  const [selectedCurrencyName, setSelectedCurrencyName] = useState<string>(
+    symbols[Object.keys(symbols)[0]] //first value of symbols object
+  );
+  const [selectedCurrencySymbol, setSelectedCurrencySymbol] = useState<string>(
+    Object.keys(symbols)[0] //first key of symbols object
+  );
+  const [enteredAmount, setEnteredAmount] = useState<number>(0);
+
+  const options = Object.keys(symbols).map((key) => (
+    <option key={symbols[key]} value={symbols[key]}>
+      {symbols[key]}
+    </option>
+  ));
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    Object.keys(symbols).map((key) => {
+      if (symbols[key] == e.target.value) {
+        setSelectedCurrencySymbol(key);
+        return;
+      }
+    });
+
+    setSelectedCurrencyName(e.target.value);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredAmount(parseInt(e.target.value));
+  };
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div
+      className={`${
+        down && "relative"
+      } bg-white w-2/3 rounded-lg p-4 h-fit shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]`}
+    >
+      <div>
+        <h2 className="font-bold text-gray-400">Select your currency</h2>
+        <div className="flex gap-2 items-center bg-[#edf0f9] rounded-lg p-2 w-full">
+          <div>flag</div>
+          <select
+            className="text-right w-full outline-none px-4 py-2 rounded-md bg-transparent appearance-none"
+            onChange={handleSelectChange}
+          >
+            {options}
+          </select>
+          <IoIosArrowDown />
+        </div>
+      </div>
+
+      <div className="mt-4 text-gray-400">
+        <h2 className="font-bold">{label}</h2>
+        <div className="flex gap-2 items-center">
+          <p className="font-semibold">{selectedCurrencySymbol}</p>
+          {!down ? (
+            <input
+              value={enteredAmount}
+              type="number"
+              onChange={handleInputChange}
+              className="no-arrows outline-none text-right w-full h-full bg-[#edf0f9] rounded-lg p-2 font-bold text-black text-2xl"
+            />
+          ) : (
+            <div className="no-arrows outline-none text-right w-full h-full bg-[#edf0f9] rounded-lg p-2 font-bold text-black text-2xl">
+              {calculatedCurrency}
+            </div>
+          )}
+        </div>
+      </div>
+      {down && (
+        <div className="absolute right-1 -top-6 w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+          <CgArrowsExchangeAltV size={25} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ExchangeCard;
--- pages/converter.tsx
+++ pages/converter.tsx
@@ -1,7 +1,21 @@
+import React, { useState } from "react";
+import ExchangeCard from "@/components/exchangeCard";
+
 export default function Converter() {
   return (
-    <div className="bg-red-200 flex-1 p-2">
-      <div className="w-full h-full bg-green-200">Converter</div>
+    <div className="flex-1 p-2 bg-gray-200">
+      <div className="w-full h-full flex flex-col ">
+        <p className="text-2xl font-semibold">Converter</p>
+        <div className="flex-1 flex items-center justify-center">
+          <div className="mt-5 flex-1 flex justify-center gap-2 flex-col items-center">
+            <ExchangeCard label="Enter amount" />
+            <ExchangeCard label="Amount" down calculatedCurrency={0} />
+            <button className="w-2/3 bg-[#705adf] mt-2 py-3 px-2 rounded-lg text-lg font-bold shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] text-white">
+              Buy 
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
